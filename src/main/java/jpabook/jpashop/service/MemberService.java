107ac5fb4aecc2557360bbb63745fd645dd4b80c
hoa0217 <gjwjdghk123@naver.com>
2023-05-13 22:18:26 +0900

    feature: 회원 수정 API

--- src/main/java/jpabook/jpashop/service/MemberService.java
+++ src/main/java/jpabook/jpashop/service/MemberService.java
@@ -1,62 +1,68 @@
 package jpabook.jpashop.service;
 
+import java.util.List;
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
-
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class MemberService {
 
-    private final MemberRepository memberRepository;
+  private final MemberRepository memberRepository;
 
-    /**
-     * 회원 가입
-     *
-     * @param member
-     * @return
-     */
-    @Transactional
-    public Long join(Member member){
-        validateDuplicateMember(member); // 중복 회원 검증
-        memberRepository.save(member); // em.persist 하게 되면 영속성 컨텍스트에 member를 올림. 그때 key값이 pk(id)가 되고 값도 넣어준다.
-        return member.getId(); // id를 돌려줘야 뭐를 저장했는지 알 수 있음.
+  /**
+   * 회원 가입
+   *
+   * @param member
+   * @return
+   */
+  @Transactional
+  public Long join(Member member) {
+    validateDuplicateMember(member); // 중복 회원 검증
+    memberRepository
+        .save(member); // em.persist 하게 되면 영속성 컨텍스트에 member를 올림. 그때 key값이 pk(id)가 되고 값도 넣어준다.
+    return member.getId(); // id를 돌려줘야 뭐를 저장했는지 알 수 있음.
+  }
+
+  /**
+   * 중복 회원 검증 로직
+   *
+   * @param member
+   */
+  private void validateDuplicateMember(Member member) {
+    List<Member> findMembers = memberRepository.findByName(member.getName());
+    if (!findMembers.isEmpty()) {
+      throw new IllegalStateException("이미 존재하는 회원입니다.");
     }
 
-    /**
-     * 중복 회원 검증 로직
-     * @param member
-     */
-    private void validateDuplicateMember(Member member) {
-        List<Member> findMembers = memberRepository.findByName(member.getName());
-        if(!findMembers.isEmpty()){
-            throw new IllegalStateException("이미 존재하는 회원입니다.");
-        }
+  }
 
-    }
+  /**
+   * 회원 전체조회
+   *
+   * @return
+   */
+  public List<Member> findMembers() {
+    return memberRepository.findAll();
+  }
 
-    /**
-     * 회원 전체조회
-     *
-     * @return
-     */
-    public List<Member> findMembers(){
-        return memberRepository.findAll();
-    }
+  /**
+   * 회원 단건조회
+   *
+   * @param id
+   * @return
+   */
+  public Member findOne(Long id) {
+    return memberRepository.findOne(id);
+  }
 
-    /**
-     * 회원 단건조회
-     *
-     * @param id
-     * @return
-     */
-    public Member findOne(Long id){
-        return memberRepository.findOne(id);
-    }
-
+  @Transactional
+  public void update(Long id, String name) {
+    Member member = memberRepository.findOne(id);
+    member.setName(name);
+  }
 }
