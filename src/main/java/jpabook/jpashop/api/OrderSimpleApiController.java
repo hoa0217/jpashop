40ef39b9faf0cd7274e70b3cd6a09341c94e000b
hoa0217 <gjwjdghk123@naver.com>
2023-05-14 02:33:16 +0900

    feature: 간단한 주문 조회 V3: 페치 조인 최적화

--- src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -25,7 +25,7 @@ public class OrderSimpleApiController {
   @GetMapping("/api/v1/simple-orders")
   public List<Order> ordersV1() {
     List<Order> all = orderRepository.findAllByString(new OrderSearch());
-    for (Order order : all){
+    for (Order order : all) {
       order.getMember().getName(); // Lazy 강제 초기화
       order.getDelivery().getAddress(); // Lazy 강제 초기화
     }
@@ -36,6 +36,13 @@ public class OrderSimpleApiController {
   public List<SimpleOrderDto> ordersV2() {
     // N + 1 문제 -> 1 + 회원 N + 배송 N -> 5
     return orderRepository.findAllByString(new OrderSearch()).stream()
+        .map(SimpleOrderDto::new)
+        .collect(Collectors.toList());
+  }
+
+  @GetMapping("/api/v3/simple-orders")
+  public List<SimpleOrderDto> ordersV3() {
+    return orderRepository.findAllWithMemberDelivery().stream()
         .map(SimpleOrderDto::new)
         .collect(Collectors.toList());
   }
